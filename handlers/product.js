8bdca3072173b22291a4c0f85886f3667d086644
balov <alexandre.balov@gmail.com>
2018-06-09 10:36:38 -0700

    Some bugs fixing

--- handlers/product.js
+++ handlers/product.js
@@ -2,9 +2,11 @@ const url = require('url')
 const fs = require('fs')
 const path = require('path')
 const qs = require('querystring')
-const multiparty = require('multiparty')
+const formidable = require('formidable')
 const shortid = require('shortid')
 const Product = require('../models/Product')
+const Category = require('../models/Category')
+const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = (req, res) => {
     req.pathname = req.pathname || url.parse(req.url).pathname
@@ -22,64 +24,66 @@ module.exports = (req, res) => {
                 res.end()
                 return
             }
-    
-            res.writeHead(200, {
-                'Content-Type' : 'text/html'
+       
+            Category.find().then((categories) => {
+                let replacement = '<select class="input-field" name="category">'
+                for(let category of categories){
+                    replacement += `$<option value="${category._id}">${category.name}</option>`
+                }
+                replacement += '</select>'
+
+                let html = data.toString().replace('{categories}', replacement)
+
+                res.writeHead(200, {
+                    'Content-Type' : 'text/html'
+                })
+                res.write(html)
+                res.end()
             })
-    
-            res.write(data)
-            res.end()
         })   
     } else if(req.pathname === '/product/add' && req.method === 'POST'){       
-        let form = new multiparty.Form()
-        let product = {}
-
-        form.on('part', (part) => {
-            if(part.filename){
-                let dataString = ''
-                part.setEncoding('binary')
-
-                part.on('data', (data) => {
-                    dataString += data
-                })
-
-                part.on('end', () => {
-                    let fileName = shortid.generate()
-                    let filePath = `/content/images/${fileName}`
-
-                    product.image = filePath
-                    fs.writeFile(`.${filePath}`, dataString, {encoding: 'ascii'}, (err) => {
-                        if(err){
-                            console.log(err)
-                            return
-                        }
-                    })
-                })
-
-            } else {
-                part.setEncoding('utf-8')
-                let field = ''
-
-                part.on('data', (data) => {
-                    field += data
-                })
-
-                part.on('end', () => {
-                    product[part.name] = field
-                })
+        let form = new formidable.IncomingForm()
+        form.parse(req, (err, fields, files) => {
+            if(err){
+                console.log(err)
+                return
             }
-        })
+            let file = files.image
+            let tempPath = file.path 
+            let fileName = shortid.generate()
 
-        form.on('close', () => {
-            Product.create(product).then(() => {
-                res.writeHead(302, {
-                    Location: '/'
-            })
-            res.end()            
+            fs.readFile(tempPath, (err, data) => {
+                if(err){
+                    console.log(err)
+                    return
+                }
+                let newFilePath = './content/images/' + fileName
+                fs.writeFile(newFilePath, data, {encoding: 'ascii'}, (err) => {
+                    if(err){
+                        console.log(err)
+                        return
+                    }
+
+                    let product = {
+                        name: fields.name,
+                        description: fields.description,
+                        price: fields.price,
+                        image: '/content/images/' + fileName,
+                        category: ObjectId(fields.category)
+                    }
+                    Product.create(product).then(insertedProduct => {
+                        Category.findById(product.category).then(cat => {
+                            cat.products.push(insertedProduct._id)
+                            cat.save()
+                            res.writeHead(302, {
+                                Location: '/'
+                        })
+                        res.end()
+                        })              
+                    })   
+                })               
             })           
-        })
-        
-        form.parse(req)           
+        })                
     } else {
         return true
     }
